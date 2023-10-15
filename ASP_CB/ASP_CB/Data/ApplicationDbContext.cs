24b77754cdb4a567902d2278ad9e624c9451f41c
Nikodem Nikiel <76944706+VeliverX@users.noreply.github.com>
2023-10-16 00:14:18 +0200

    nie działa

--- ASP_CB/ASP_CB/Data/ApplicationDbContext.cs
+++ ASP_CB/ASP_CB/Data/ApplicationDbContext.cs
@@ -1,13 +1,17 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using ASP_CB.Areas.Admin.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace ASP_CB.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser, ApplicationRole, int>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
         }
+        public DbSet<ApplicationRole> Roles { get; set; }
+        public DbSet<ApplicationUser> Users { get; set; }
+
     }
 }
--- ASP_CB/ASP_CB/Program.cs
+++ ASP_CB/ASP_CB/Program.cs
@@ -1,3 +1,4 @@
+using ASP_CB.Areas.Admin.Models;
 using ASP_CB.Data;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
@@ -10,17 +11,22 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-    .AddEntityFrameworkStores<ApplicationDbContext>();
+builder.Services.AddIdentity<ApplicationUser, ApplicationRole>()
+                .AddEntityFrameworkStores<ApplicationDbContext>().
+                AddDefaultUI().AddDefaultTokenProviders();
+
+//builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+//    .AddEntityFrameworkStores<ApplicationDbContext>();
+
 builder.Services.AddRazorPages();
 
 builder.Services.Configure<IdentityOptions>(options =>
 {
     // Default Password settings.
-    options.Password.RequireDigit = false;
+    options.Password.RequireDigit = true;
     options.Password.RequireLowercase = false;
-    options.Password.RequireNonAlphanumeric = false;
-    options.Password.RequireUppercase = false;
+    options.Password.RequireNonAlphanumeric = true;
+    options.Password.RequireUppercase = true;
     options.Password.RequiredLength = 8;
     options.Password.RequiredUniqueChars = 1;
 });
@@ -47,4 +53,53 @@ app.UseAuthorization();
 
 app.MapRazorPages();
 
-app.Run();
+using (var scope = app.Services.CreateScope())
+{
+
+    var _userManager = scope.ServiceProvider.GetService<UserManager<ApplicationUser>>();
+    var _roleManager = scope.ServiceProvider.GetService<RoleManager<ApplicationRole>>();
+
+    var createAdminRole = await _roleManager.CreateAsync(
+        new ApplicationRole()
+        {
+            Name = "Administrator"
+        });
+
+    var createUserRole = await _roleManager.CreateAsync(
+        new ApplicationRole()
+        {
+            Name = "User"
+        });
+
+
+    var createResult = await _userManager.CreateAsync(
+    new ApplicationUser()
+    {
+        UserName = "admin@admin.com",
+        Email = "admin@admin.com",
+        LockoutEnabled = false,
+        AccessFailedCount = 0,
+
+
+
+    }, "Haslo123!");
+
+    var createUser = await _userManager.CreateAsync(
+    new ApplicationUser()
+    {
+        UserName = "nikodem@ubb.edu",
+        Email = "nikodem@ubb.edu",
+        LockoutEnabled = false,
+        AccessFailedCount = 0,
+
+
+
+    }, "Haslo123!");
+
+    var adminUser =  _userManager.FindByNameAsync("admin@admin.com").Result;
+    var User = _userManager.FindByNameAsync("nikodem@ubb.edu").Result;
+    _userManager.AddToRoleAsync(adminUser, "Administrator");
+    _userManager.AddToRoleAsync(User, "User");
+}
+
+    app.Run();
