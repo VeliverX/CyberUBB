f661bbfce2baf0ec817e4c54e8455e61e71510c0
Nikodem Nikiel <76944706+VeliverX@users.noreply.github.com>
2023-11-06 19:04:04 +0100

    głupoty

--- AspCyber/AspCyber/wwwroot/js/idle.js
+++ AspCyber/AspCyber/wwwroot/js/idle.js
@@ -1,4 +1,4 @@
-let inactivityTimeout = 5 * 1000; // 10 sekund w milisekundach
+let inactivityTimeout = 5 * 1000;
 let timeoutId;
 
 function resetTimeout() {
@@ -7,32 +7,25 @@ function resetTimeout() {
 }
 
 function logoutUser() {
-    var form = document.getElementById("LogoutBtn"); // Zamiast "YourFormId" podaj w³aœciwe ID formularza
+    var form = document.getElementById("LogoutBtn"); 
     if (form) {
         console.log("Znaleziono formularz.");
 
         form.addEventListener("submit", function (event) {
-            // Zapobiegaj domyœlnemu zachowaniu formularza (zapobiega przekierowaniu na inn¹ stronê)
-            event.preventDefault();
-
-            // Tutaj mo¿esz dodaæ kod wylogowania u¿ytkownika lub inny odpowiedni kod
+            
+            event.preventDefault();   
             console.log("U¿ytkownik zosta³ wylogowany po klikniêciu przycisku LogoutBtn.");
-
-            // Na przyk³ad przekieruj u¿ytkownika na inn¹ stronê
-            // window.location.href = '/Identity/Account/Logout';
         });
     } else {
         console.log("Nie znaleziono formularza.");
     }
 }
-
 function logInactiveTime() {
     const currentTime = new Date();
     const lastActivityTime = new Date(currentTime - inactivityTimeout);
     console.log(`Czas nieaktywnoœci: ${inactivityTimeout / 1000} sekund. Ostatnia aktywnoœæ: ${lastActivityTime}`);
 }
 
-// Dodaj obs³ugê zdarzeñ dla aktywnoœci myszki i klawiatury
 document.addEventListener("mousemove", function () {
     resetTimeout();
     logInactiveTime();
@@ -43,5 +36,4 @@ document.addEventListener("keydown", function () {
     logInactiveTime();
 });
 
-// Inicjalizuj timer po za³adowaniu strony
 resetTimeout();
